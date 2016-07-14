45664b3cecb21bd2a23119c3d714bc5329c3bff5
mcheli <mpcheli7@gmail.com>
2016-07-14 17:57:09 -0400

    Added simple example of looping through array of objects

---
+++ ArrayObjectLoop.js
@@ -0,0 +1,28 @@
+//Instantiate the array of objects
+var objectArray = [
+    {
+        name: "First",
+        condition: false
+    },
+    {
+        name: "Second",
+        condition: true
+    },
+    {
+        name: "Third",
+        condition: false
+    },
+    {
+        name: "Fourth",
+        condition: true
+    }];
+
+for (var i = 0; i < objectArray.length; i++) { //Loop through the array
+    var object = objectArray[i]; //Examine single object
+    if (object.condition) { //Evaluate condition
+        object.newAttribute = "Added a new attribute"; //Add the new attribute to the object
+    }
+}
+
+//Print the result of transformation
+console.log(JSON.stringify(objectArray, undefined, 2));
